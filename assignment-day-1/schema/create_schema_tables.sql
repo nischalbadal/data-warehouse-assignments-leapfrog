bbe80c81434827b3c3f867de766b11b4e2a05434
Nischal Badal <nischalsharma541@gmail.com>
2021-09-14 11:09:51 +0545

    adding all files

--- assignment-day-1/schema/create_schema_tables.sql
+++ assignment-day-1/schema/create_schema_tables.sql
@@ -12,3 +12,48 @@ create table employee.employee(
     fte FLOAT NOT NULL,
     location VARCHAR(200)
 );
+
+create table employee.department(
+  department_id INT PRIMARY KEY,
+  department_name VARCHAR(200),
+  manager_employee_id INT,
+  constraint fk_manager_employee_id
+        FOREIGN KEY (manager_employee_id)
+                REFERENCES employee.employee(employee_id)
+);
+drop table employee.timesheet;
+
+create table employee.timesheet(
+  timesheet_id SERIAL PRIMARY KEY,
+  employee_id INT NOT NULL,
+  cost_center INT NOT NULL,
+  punch_in_time TIMESTAMP default now(),
+  punch_out_time TIMESTAMP default now(),
+  punch_apply_date DATE default current_date NOT NULL ,
+  paycode VARCHAR(10) NOT NULL,
+  constraint fk_employee_id
+        FOREIGN KEY (employee_id)
+                REFERENCES employee.employee(employee_id),
+  constraint fk_cost_center
+        FOREIGN KEY (cost_center)
+                REFERENCES employee.department(department_id)
+);
+
+create table employee.employee_records(
+    record_id SERIAL PRIMARY KEY,
+    employee_id INT NOT NULL,
+    timesheet_id INT NOT NULL,
+    department_id INT NOT NULL,
+    shift_name VARCHAR(200),
+    salary FLOAT NOT NULL,
+    work_hour FLOAT NOT NULL,
+    constraint fk_employee_id
+        FOREIGN KEY (employee_id)
+                REFERENCES employee.employee(employee_id),
+    constraint fk_timesheet_id
+        FOREIGN KEY (timesheet_id)
+                REFERENCES employee.timesheet(timesheet_id),
+    constraint fk_department_id
+        FOREIGN KEY (department_id)
+                REFERENCES employee.department(department_id)
+);
