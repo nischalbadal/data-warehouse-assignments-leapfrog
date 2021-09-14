b92ef8845c0ad1e5667f928ad41f895ea97acc3c
Nischal Badal <nischalsharma541@gmail.com>
2021-09-14 13:58:01 +0545

    finalizing minor leftovers in assignment

--- assignment-day-1/schema/create_schema_tables.sql
+++ assignment-day-1/schema/create_schema_tables.sql
@@ -1,59 +1,52 @@
 create schema  employee;
 
+create table employee.department(
+  department_id INT PRIMARY KEY,
+  department_name VARCHAR(200)
+);
+
 create table employee.employee(
     employee_id INT PRIMARY KEY ,
+    department_id INT NOT NULL,
+    manager_employee_id INT,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL ,
     employee_role VARCHAR(100) NOT NULL,
     hire_date DATE NOT NULL,
     terminated_date DATE default '01-01-1700',
     terminated_reason VARCHAR(500),
+    salary FLOAT NOT NULL,
     dob DATE NOT NULL ,
     fte FLOAT NOT NULL,
-    location VARCHAR(200)
-);
-
-create table employee.department(
-  department_id INT PRIMARY KEY,
-  department_name VARCHAR(200),
-  manager_employee_id INT,
-  constraint fk_manager_employee_id
+    location VARCHAR(200),
+    constraint fk_department_id
+        FOREIGN KEY (department_id)
+                REFERENCES employee.department(department_id),
+    constraint fk_manager_employee_id
         FOREIGN KEY (manager_employee_id)
                 REFERENCES employee.employee(employee_id)
 );
-drop table employee.timesheet;
 
-create table employee.timesheet(
-  timesheet_id SERIAL PRIMARY KEY,
+create table employee.punch_date(
+    id SERIAL PRIMARY KEY,
+    punch_apply_date DATE NOT NULL,
+    day_of_week VARCHAR(100)
+
+);
+create table employee.employee_attendance(
+  attendance_id SERIAL PRIMARY KEY,
   employee_id INT NOT NULL,
   cost_center INT NOT NULL,
-  punch_in_time TIMESTAMP default now(),
-  punch_out_time TIMESTAMP default now(),
-  punch_apply_date DATE default current_date NOT NULL ,
+  punch_in_time TIMESTAMP,
+  punch_out_time TIMESTAMP,
+  working_shift VARCHAR(200),
+  work_hour FLOAT NOT NULL,
+  punch_apply_date INT NOT NULL,
   paycode VARCHAR(10) NOT NULL,
   constraint fk_employee_id
         FOREIGN KEY (employee_id)
                 REFERENCES employee.employee(employee_id),
-  constraint fk_cost_center
-        FOREIGN KEY (cost_center)
-                REFERENCES employee.department(department_id)
-);
-
-create table employee.employee_records(
-    record_id SERIAL PRIMARY KEY,
-    employee_id INT NOT NULL,
-    timesheet_id INT NOT NULL,
-    department_id INT NOT NULL,
-    shift_name VARCHAR(200),
-    salary FLOAT NOT NULL,
-    work_hour FLOAT NOT NULL,
-    constraint fk_employee_id
-        FOREIGN KEY (employee_id)
-                REFERENCES employee.employee(employee_id),
-    constraint fk_timesheet_id
-        FOREIGN KEY (timesheet_id)
-                REFERENCES employee.timesheet(timesheet_id),
-    constraint fk_department_id
-        FOREIGN KEY (department_id)
-                REFERENCES employee.department(department_id)
+  constraint fk_punch_apply_date
+        FOREIGN KEY (punch_apply_date)
+                REFERENCES employee.punch_date(id)
 );
