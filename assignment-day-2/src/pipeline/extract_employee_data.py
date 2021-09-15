48ea8733251b09f0331e4cc3dbb793c719b48482
Nischal Badal <nischalbadalofficial@gmail.com>
2021-09-15 13:41:21 +0545

    fixing a typo

--- assignment-day-2/src/pipeline/extract_employee_data.py
+++ assignment-day-2/src/pipeline/extract_employee_data.py
@@ -50,6 +50,7 @@ try:
         root = etree.parse(fileName)
         for i in root.findall("Employee"):
             values = [i.find(n).text for n in ("employee_id", "first_name", "last_name", "department_id", "department_name","manager_employee_id","employee_role","salary","hire_date","terminated_date","terminated_reason","dob","fte","location")]
+            values = ['' if v is None else v for v in values]
             value_str = "(" + str(values)[1:-1] + ");"
             insert_query = '''
                             INSERT INTO raw_employee ("employee_id", "first_name", "last_name", "department_id", "department_name","manager_employee_id","employee_role","salary","hire_date","terminated_date","terminated_reason","dob","fte","location") 
