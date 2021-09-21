c8ee526c6dfd3aae6a37a4305ba7afb4bf4bd492
Nischal Badal <nischalsharma541@gmail.com>
2021-09-21 12:43:09 +0545

    adding extraction scripts

--- weekend-assignment/src/pipeline/extract_customer_data.py
+++ weekend-assignment/src/pipeline/extract_customer_data.py
@@ -2,7 +2,7 @@ from utils import *
 
 try:
    
-    def extract_sales_data(fileName, con, cur):
+    def extract_customer_data(fileName, con, cur):
         with open(fileName, 'r') as f:
             i = 0
             for line in f:
@@ -10,29 +10,29 @@ try:
                     i+=1
                     continue
                 row = line[:-1].split(",")
-                with open("../sql/queries/extract_raw_sales_data.sql") as f:
+                with open("../sql/queries/extract_raw_customer_data.sql") as f:
                     insert_query = ' '.join(map(str, f.readlines())) 
                     cur.execute(insert_query, row)     
                     con.commit()
-        print("Extraction successful to raw_sales table.") 
+        print("Extraction successful to raw_customer table.") 
     
        
-    def archive_sales_data(con, cur):
-        with open("../sql/queries/copy_raw_sales.sql") as f:
+    def archive_customer_data(con, cur):
+        with open("../sql/queries/copy_raw_customer.sql") as f:
             sql = ' '.join(map(str, f.readlines())) 
             cur.execute(sql)
             con.commit()
-        print("Archiving successful to copy_raw_sales table.") 
+        print("Archiving successful to copy_raw_customer table.") 
 
 
     def main():
         con = connect()
         cur = con.cursor()
 
-        truncate_table("raw_sales", con, cur)
-        truncate_table("copy_raw_sales", con, cur)
+        truncate_table("raw_customer", con, cur)
+        truncate_table("copy_customer_sales", con, cur)
 
-        extract_sales_data("../../data/sales_dump.csv",con,cur)
+        extract_customer_data("../../data/customer_dump.csv",con,cur)
      
 
         cur.close()
--- weekend-assignment/src/pipeline/extract_product_data.py
+++ weekend-assignment/src/pipeline/extract_product_data.py
@@ -2,7 +2,7 @@ from utils import *
 
 try:
    
-    def extract_sales_data(fileName, con, cur):
+    def extract_product_data(fileName, con, cur):
         with open(fileName, 'r') as f:
             i = 0
             for line in f:
@@ -10,29 +10,29 @@ try:
                     i+=1
                     continue
                 row = line[:-1].split(",")
-                with open("../sql/queries/extract_raw_sales_data.sql") as f:
+                with open("../sql/queries/extract_raw_product_data.sql") as f:
                     insert_query = ' '.join(map(str, f.readlines())) 
                     cur.execute(insert_query, row)     
                     con.commit()
-        print("Extraction successful to raw_sales table.") 
+        print("Extraction successful to raw_product table.") 
     
        
     def archive_sales_data(con, cur):
-        with open("../sql/queries/copy_raw_sales.sql") as f:
+        with open("../sql/queries/copy_raw_product.sql") as f:
             sql = ' '.join(map(str, f.readlines())) 
             cur.execute(sql)
             con.commit()
-        print("Archiving successful to copy_raw_sales table.") 
+        print("Archiving successful to copy_raw_product table.") 
 
 
     def main():
         con = connect()
         cur = con.cursor()
 
-        truncate_table("raw_sales", con, cur)
-        truncate_table("copy_raw_sales", con, cur)
+        truncate_table("raw_product", con, cur)
+        truncate_table("copy_product_sales", con, cur)
 
-        extract_sales_data("../../data/sales_dump.csv",con,cur)
+        extract_product_data("../../data/product_dump.csv",con,cur)
      
 
         cur.close()
